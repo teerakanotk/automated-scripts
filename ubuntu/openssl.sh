a716696997ad4282bb6d4d02c93ad5199b5d5f4c
Teerakan Otklan <161741743+teerakanotk@users.noreply.github.com>
2025-06-23 07:43:07 +0700

    chore: update hostname -I to hostname --fqdn

--- ubuntu/openssl.sh
+++ ubuntu/openssl.sh
@@ -3,9 +3,9 @@
 # Date: 2025-06-21
 # App: Openssl
 
-SERVER_NAME=$(hostname -I | awk '{print $1}')
+SERVER_NAME=$(hostname --fqdn | awk '{print $1}')
 
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/ssl/private/self-signed.key \
     -out /etc/ssl/certs/self-signed.crt \
-    -subj "/CN=$SERVER_NAME"
+    -subj "/CN=$SERVER_NAME/O=Self-signed"
