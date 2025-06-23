0a9042cfa01c48d278a0ec5d1595ef667f913a02
Teerakan Otklan <161741743+teerakanotk@users.noreply.github.com>
2025-06-23 07:38:06 +0700

    chore(ubuntu/openssl-gen.sh): rename ssl-selfsigned to self-signed in script

--- ubuntu/openssl-gen.sh
+++ ubuntu/openssl-gen.sh
@@ -6,6 +6,6 @@
 SERVER_NAME=$(hostname -I | awk '{print $1}')
 
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/private/ssl-selfsigned.key \
-    -out /etc/ssl/certs/ssl-selfsigned.crt \
+    -keyout /etc/ssl/private/self-signed.key \
+    -out /etc/ssl/certs/self-signed.crt \
     -subj "/CN=$SERVER_NAME"
