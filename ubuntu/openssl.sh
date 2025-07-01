d7af1cc48b679fe9aa639fdcc65b92566ea643e1
Teerakan Otklan <161741743+teerakanotk@users.noreply.github.com>
2025-07-02 03:55:34 +0700

    Update openssl.sh

--- ubuntu/openssl.sh
+++ ubuntu/openssl.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
-
-# Date: 2025-06-21
-# App: Openssl
-
 SERVER_NAME=$(hostname --fqdn | awk '{print $1}')
 
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/private/self-signed.key \
-    -out /etc/ssl/certs/self-signed.crt \
+    -keyout /etc/ssl/private/selfsigned.key \
+    -out /etc/ssl/certs/selfsigned.crt \
     -subj "/CN=$SERVER_NAME"
